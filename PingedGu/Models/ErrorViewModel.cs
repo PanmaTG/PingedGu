1b229a419eed127b495c781f8c5d3f1fe3ffa7a9
PanmaTG <neildavid618@gmail.com>
2026-01-28 21:14:35 +0800

    Add project files.

---
+++ PingedGu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PingedGu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
