3d3375cb52328587244d13169186840f32f36640
PanmaTG <neildavid618@gmail.com>
2026-02-09 21:02:54 +0800

    Created WebAppDbContext.cs and added code for database and Entity framework

---
+++ PingedGu/Data/WebAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PingedGu.Data
+{
+    public class WebAppDbContext:DbContext
+    {
+        // Constructor Method. WebAppDbContext used as a parameter inside <>.
+        // :base keyword points to the :DbContext 
+        public WebAppDbContext(DbContextOptions<WebAppDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
+ 
--- PingedGu/Program.cs
+++ PingedGu/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PingedGu.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(); 
 
+//Database related stuffs
+string dbConnectionString = builder.Configuration.GetConnectionString("Default");
+
+//WebAppDbContext is the name of the class I created inside the Data folder
+builder.Services.AddDbContext<WebAppDbContext>(options => options.UseSqlServer(dbConnectionString));
+
+//---------------
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
