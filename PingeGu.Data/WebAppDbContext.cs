9d5e17c995c0b288eb80d58902963cca39441ad7
PanmaTG <neildavid618@gmail.com>
2026-02-10 20:49:57 +0800

    Added WebAppDbContext class for bridging the model and the database

---
+++ PingeGu.Data/WebAppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PingedGu.Data.Models;
+
+
+namespace PingedGu.Data
+{
+    public class WebAppDbContext:DbContext
+    {
+        // Constructor Method. WebAppDbContext used as a parameter inside <>.
+        // :base keyword points to the :DbContext 
+        public WebAppDbContext(DbContextOptions<WebAppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Posts)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId);
+        }
+    }
+}
+ 
