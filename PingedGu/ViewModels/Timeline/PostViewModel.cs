72c9d565da2b81b14f6c77dae772b4d98bcf5523
PanmaTG <neildavid618@gmail.com>
2026-02-15 23:03:36 +0800

    Created a View Model for post

--- PingedGu/ViewModels/Timeline/PostViewModel.cs
+++ PingedGu/ViewModels/Timeline/PostViewModel.cs
@@ -3,5 +3,8 @@
     public class PostViewModel
     {
         public string Content { get; set; }
+
+        // IFormFile → a property for sending an image file via HTTPS request
+        public IFormFile Image {  get; set; }
     }
 }
