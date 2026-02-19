afc0e4706dd69291a7cd9d0ae90756875f2e51df
PanmaTG <neildavid618@gmail.com>
2026-02-19 17:40:16 +0800

    Updated database structure with the addition of Comments table

--- PingeGu.Data/WebAppDbContext.cs
+++ PingeGu.Data/WebAppDbContext.cs
@@ -16,6 +16,7 @@ namespace PingedGu.Data
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -24,7 +25,7 @@ namespace PingedGu.Data
                 .WithOne(p => p.User)
                 .HasForeignKey(p => p.UserId);
 
-            
+            //Likes
             modelBuilder.Entity<Like>()
                 .HasKey(l => new { l.PostId, l.UserId });
 
@@ -37,6 +38,19 @@ namespace PingedGu.Data
             modelBuilder.Entity<Like>()
                 .HasOne(l => l.User)
                 .WithMany(u => u.Likes)
+                .HasForeignKey(l => l.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Comments
+            modelBuilder.Entity<Comment>()
+                .HasOne(l => l.Post)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(l => l.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(l => l.User)
+                .WithMany(u => u.Comments)
                 .HasForeignKey(l => l.UserId)
                 .OnDelete(DeleteBehavior.Restrict);
 
