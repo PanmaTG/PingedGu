f5f5c0dd6ad0acd37cfce1b4d91ee449aa5b0272
PanmaTG <neildavid618@gmail.com>
2026-02-22 02:40:20 +0800

    feat (post-comment/reply): add com/reply functionality under posts
    Also change com/reply count value and add datetime when the com/reply was created

--- PingedGu/Controllers/HomeController.cs
+++ PingedGu/Controllers/HomeController.cs
@@ -29,6 +29,7 @@ namespace PingedGu.Controllers
             var allPosts = await _context.Posts
                 .Include(n => n.User)
                 .Include(n => n.Likes)
+                .Include(n => n.Comments).ThenInclude(n => n.User)
                 .OrderByDescending(n => n.DateCreated)
                 .ToListAsync();
             //
