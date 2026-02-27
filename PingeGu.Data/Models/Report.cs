31861bc7956d3a58933b2cac846624a3cee92042
PanmaTG <neildavid618@gmail.com>
2026-02-28 00:27:30 +0800

    feat(db): add reports table and ef core migration

---
+++ PingeGu.Data/Models/Report.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingedGu.Data.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        //FK = Foreign Key
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+
+        // Nav Properties
+        public Post Post { get; set; }
+        public User User { get; set; }
+    }
+}
