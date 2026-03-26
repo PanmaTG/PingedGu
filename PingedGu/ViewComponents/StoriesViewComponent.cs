fe3f7e5a210872f3922acfb3def039e522666021
PanmaTG <neildavid618@gmail.com>
2026-03-26 22:53:52 +0800

    feat(myday): add ViewComponent for myday/stories

---
+++ PingedGu/ViewComponents/StoriesViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PingedGu.ViewComponents
+{
+    public class StoriesViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> Invoke()
+        {
+            return View();
+        }
+    }
+}
