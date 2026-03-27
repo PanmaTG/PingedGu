6cfa6ac32031c1adfcfe9b490422270294406821
PanmaTG <neildavid618@gmail.com>
2026-03-27 23:53:31 +0800

    feat(myday): add AppDbContext for myday/stories

--- PingedGu/ViewComponents/StoriesViewComponent.cs
+++ PingedGu/ViewComponents/StoriesViewComponent.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PingedGu.Data;
 
 namespace PingedGu.ViewComponents
 {
     public class StoriesViewComponent : ViewComponent
     {
+        private readonly WebAppDbContext _context;
+        public StoriesViewComponent(WebAppDbContext context)
+        {
+            _context = context;
+        }
+
         public async Task<IViewComponentResult> Invoke()
         {
             return View();
         }
     }
 }
+ 
