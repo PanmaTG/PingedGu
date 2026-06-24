cc76bdd5e3dc188d26cc6d62375744288688e3c7
PanmaTG <neildavid618@gmail.com>
2026-06-24 23:44:19 +0800

    feat(trending): convert trendings partial view to view component

---
+++ PingedGu/ViewComponents/TrendingsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PingedGu.ViewComponents
+{
+    public class TrendingsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
