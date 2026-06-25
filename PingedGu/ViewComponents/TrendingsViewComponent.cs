a2a10bf9d10256bc26e988d49ccb00e755039c14
PanmaTG <neildavid618@gmail.com>
2026-06-25 22:32:20 +0800

    feat(trending): update the trending view component to load the trendings from the database

--- PingedGu/ViewComponents/TrendingsViewComponent.cs
+++ PingedGu/ViewComponents/TrendingsViewComponent.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PingedGu.Data;
 
 namespace PingedGu.ViewComponents
 {
     public class TrendingsViewComponent : ViewComponent
     {
+        private readonly WebAppDbContext _context;
+        public TrendingsViewComponent(WebAppDbContext context)
+        {
+            _context = context;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var oneWeekAgoNow = DateTime.UtcNow.AddDays(-7);
+
+            var top3Trendings = await _context.Trendings
+                .Where(h => h.DateCreated > oneWeekAgoNow)
+                .OrderByDescending(n => n.Count)
+                .Take(3)
+                .ToListAsync();
+
+            return View(top3Trendings);
         }
     }
 }
