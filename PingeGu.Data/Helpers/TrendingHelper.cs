34c83b963a609ad998c1473d076bbb0540a4a095
PanmaTG <neildavid618@gmail.com>
2026-06-25 21:59:29 +0800

    feat(trending): add a funtion that scans a user post for hashtags

---
+++ PingeGu.Data/Helpers/TrendingHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PingedGu.Data.Helpers
+{
+    public static class TrendingHelper
+    {
+        public static List<string> GetTrendings(string postText)
+        {
+            var trendingPattern = new Regex(@"#\w+");
+            var matches = trendingPattern.Matches(postText)
+                .Select(match => match.Value.TrimEnd('.', ',', '!', '?'))
+                .Distinct()
+                .ToList();
+
+            return matches;
+        }
+    }
+}
