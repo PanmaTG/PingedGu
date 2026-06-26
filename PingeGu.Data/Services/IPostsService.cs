0916bdbca2cbc7cd9d79e144cfbfc2b2e732a71b
PanmaTG <neildavid618@gmail.com>
2026-06-26 22:37:58 +0800

    feat(service): add interface for posts and class for transitioning to service oriented architecture

---
+++ PingeGu.Data/Services/IPostsService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using PingedGu.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingedGu.Data.Services
+{
+    public interface IPostsService
+    {
+        Task<List<Post>> GetAllPostsAsync(int loggedInUserId);
+        Task<Post> CreatePostAsync(Post post, IFormFile Image);
+        Task RemovePostAsync(int postId);
+
+        Task AddPostCommentAsync(Comment comment);
+        Task RemovePostCommentAsync(int commentId);
+
+        Task TogglePostLikeAsync(int postId, int userId);
+        Task TogglePostFavoriteAsync(int postId, int userId);
+        Task TogglePostAsync(int postId, int userId);
+    }
+}
---
+++ PingeGu.Data/Services/PostsService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using PingedGu.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingedGu.Data.Services
+{
+    public class PostsService:IPostsService
+    {
+        private readonly WebAppDbContext _context;
+        public PostsService(WebAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddPostCommentAsync(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> CreatePostAsync(Post post, IFormFile Image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Post>> GetAllPostsAsync(int loggedInUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePostAsync(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePostCommentAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TogglePostAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TogglePostFavoriteAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TogglePostLikeAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
