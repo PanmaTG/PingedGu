36bd6272bacf35b06d31c552834f4d8a70d89cdb
PanmaTG <neildavid618@gmail.com>
2026-07-12 12:47:10 +0800

    feat(user-authn-authz): add controller for authentication with login and register empty views

---
+++ PingedGu/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PingedGu.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
