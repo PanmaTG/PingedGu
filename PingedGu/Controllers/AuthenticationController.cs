3077ab1afc97b2fee324f2f7a20ad1dae04a1687
PanmaTG <neildavid618@gmail.com>
2026-07-13 20:11:46 +0800

    feat(register): add methods for registering a user

--- PingedGu/Controllers/AuthenticationController.cs
+++ PingedGu/Controllers/AuthenticationController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PingedGu.Data.Helpers.Constants;
+using PingedGu.Data.Models;
+using PingedGu.ViewModels.Authentication;
 
 namespace PingedGu.Controllers
 {
     public class AuthenticationController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthenticationController(UserManager<User> userManager, SignInManager<User> signInManager) 
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public async Task<IActionResult> Login()
         {
             return View();
@@ -11,6 +24,30 @@ namespace PingedGu.Controllers
 
         public async Task<IActionResult> Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            var newUser = new User()
+            {
+                FullName = $"{registerViewModel.FirstName} {registerViewModel.LastName}",
+                Email = registerViewModel.Email,
+                UserName = registerViewModel.Email
+            };
+
+            var result = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, AppRoles.User);
+
+                await _signInManager.SignInAsync(newUser, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }   
+
+
             return View();
         }
     }
