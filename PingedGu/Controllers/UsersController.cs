9c04f6bc43ed2e51eee5e5661045e1dbe9cfacfb
PanmaTG <142467747+PanmaTG@users.noreply.github.com>
2026-07-15 20:58:39 +0800

    feat(user-profile-details): add a controller and an empty view

---
+++ PingedGu/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PingedGu.Controllers.Base;
+
+namespace PingedGu.Controllers
+{
+    public class UsersController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(string userId)
+        {
+            return View();
+        }
+    }
+}
