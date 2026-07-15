6236e4dc6d9c4527a8e4933e06cd1212ab3be199
PanmaTG <142467747+PanmaTG@users.noreply.github.com>
2026-07-15 23:12:58 +0800

    feat(user-profile-details): display proper user profile picture, full name and user name based on user data from the database

--- PingedGu/Controllers/UsersController.cs
+++ PingedGu/Controllers/UsersController.cs
@@ -1,16 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using PingedGu.Controllers.Base;
+using PingedGu.Data.Models;
 using PingedGu.Data.Services;
+using PingedGu.ViewModels.Users;
 
 namespace PingedGu.Controllers
 {
     public class UsersController : BaseController
     {
         private readonly IUsersService _usersService;
+        private readonly UserManager<User> _userManager;
 
-        public UsersController(IUsersService usersService)
+        public UsersController(IUsersService usersService, UserManager<User> userManager)
         {
             _usersService = usersService;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -20,9 +25,16 @@ namespace PingedGu.Controllers
 
         public async Task<IActionResult> Details(int userId)
         {
+            var user = await _userManager.FindByIdAsync(userId.ToString());
             var userPosts = await _usersService.GetUserPosts(userId);
 
-            return View(userPosts);
+            var userProfileViewModel = new GetUserProfileViewModel()
+            {
+                User = user,
+                Posts = userPosts
+            };
+
+            return View(userProfileViewModel);
         }
     }
 }
---
+++ PingedGu/ViewModels/Users/GetUserProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using PingedGu.Data.Models;
+
+namespace PingedGu.ViewModels.Users
+{
+    public class GetUserProfileViewModel
+    {
+        public User User { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
