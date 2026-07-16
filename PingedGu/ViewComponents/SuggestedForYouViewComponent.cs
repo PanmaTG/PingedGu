f9fdc8bc241309abb30624024b311c8165c8d097
PanmaTG <142467747+PanmaTG@users.noreply.github.com>
2026-07-16 15:48:28 +0800

    feat(followers): add SuggestedForYou view component and replace _SuggestedForYou partial views with it

---
+++ PingedGu/ViewComponents/SuggestedForYouViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PingedGu.ViewComponents
+{
+    public class SuggestedForYouViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
