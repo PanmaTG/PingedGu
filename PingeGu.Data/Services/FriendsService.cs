45e0ec90d7879dfda41f0d5c40003657eff97dba
PanmaTG <142467747+PanmaTG@users.noreply.github.com>
2026-07-16 14:46:43 +0800

    feat(followers): add a service and a method for sending follow request

---
+++ PingeGu.Data/Services/FriendsService.cs
@@ -0,0 +1,34 @@
+﻿using PingedGu.Data.Helpers.Constants;
+using PingedGu.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingedGu.Data.Services
+{
+    public class FriendsService : IFriendsService
+    {
+        private readonly WebAppDbContext _context;
+
+        public FriendsService(WebAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> SendRequest(int senderId, int receiverId)
+        {
+            var request = new FriendRequest
+            {
+                SenderId = senderId,
+                ReceiverId = receiverId,
+                Status = FriendshipStatus.Pending,
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now
+            };
+            _context.FriendRequests.Add(request);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
---
+++ PingeGu.Data/Services/IFriendsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingedGu.Data.Services
+{
+    public interface IFriendsService
+    {
+        Task<bool> SendRequest(int senderId, int receiverId);
+    }
+}
