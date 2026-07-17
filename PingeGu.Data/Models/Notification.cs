35cd0ee76b0ad6ef1317479b831e4ce25fe67706
PanmaTG <142467747+PanmaTG@users.noreply.github.com>
2026-07-17 16:26:38 +0800

    chore(notification-db): initialize db infrastructure

---
+++ PingeGu.Data/Models/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingedGu.Data.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
