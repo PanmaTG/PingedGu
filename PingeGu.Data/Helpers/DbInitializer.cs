9c74bd8a8e2e4649aa9cb111361925b675432659
PanmaTG <142467747+PanmaTG@users.noreply.github.com>
2026-07-19 18:35:09 +0800

    fix: resolve user seeding order
    Comment the seeding method out after

--- PingeGu.Data/Helpers/DbInitializer.cs
+++ PingeGu.Data/Helpers/DbInitializer.cs
@@ -29,7 +29,7 @@ namespace PingedGu.Data.Helpers
                 var userPassword = "SPTTarkov123!";
                 var newUser = new User()
                 {
-                    UserName = "panmatg",
+                    UserName = "panma.tg",
                     Email = "panma.tg@gmail.com",
                     FullName = "Panma TG",
                     PfpUrl = "https://preview.redd.it/whos-your-favorite-sora-v0-2g7mjaz3t64f1.jpg?width=554&format=pjpg&auto=webp&s=47aa5e4e7c0dd58d91743a1841db9308bd1c505a",
@@ -60,51 +60,51 @@ namespace PingedGu.Data.Helpers
         //To make this an async change void → Task
         //add async keyword before "Task" — public static async Task SeedAsync();
         //& change Seed → SeedAsync
-        public static async Task SeedAsync(WebAppDbContext webAppDbContext)
-        {
-            if (!webAppDbContext.Users.Any() && !webAppDbContext.Posts.Any())
-            {
-                var newUser = new User()
-                {
-                    FullName = "TripleEarthhh",
-                    PfpUrl = "https://preview.redd.it/whos-your-favorite-sora-v0-2g7mjaz3t64f1.jpg?width=554&format=pjpg&auto=webp&s=47aa5e4e7c0dd58d91743a1841db9308bd1c505a"
-                };
+        //public static async Task SeedAsync(WebAppDbContext webAppDbContext)
+        //{
+        //    if (!webAppDbContext.Users.Any() && !webAppDbContext.Posts.Any())
+        //    {
+        //        var newUser = new User()
+        //        {
+        //            FullName = "TripleEarthhh",
+        //            PfpUrl = "https://preview.redd.it/whos-your-favorite-sora-v0-2g7mjaz3t64f1.jpg?width=554&format=pjpg&auto=webp&s=47aa5e4e7c0dd58d91743a1841db9308bd1c505a"
+        //        };
 
-                //To make this in async add "await keyword at the beginning here
-                //change Add() → AddAsync();
-                //& change SaveChanges () → SaveChangesAsync();
-                await webAppDbContext.Users.AddAsync(newUser);
-                await webAppDbContext.SaveChangesAsync();
+        //        //To make this in async add "await keyword at the beginning here
+        //        //change Add() → AddAsync();
+        //        //& change SaveChanges () → SaveChangesAsync();
+        //        await webAppDbContext.Users.AddAsync(newUser);
+        //        await webAppDbContext.SaveChangesAsync();
 
-                var newPostWithoutImage = new Post()
-                {
-                    Content = "First post by PanmaTG lets go. 676767676",
-                    ImageUrl = "",
-                    NumOfReports = 0,
-                    DateCreated = DateTime.UtcNow,
-                    DateUpdated = DateTime.UtcNow,
+        //        var newPostWithoutImage = new Post()
+        //        {
+        //            Content = "First post by PanmaTG lets go. 676767676",
+        //            ImageUrl = "",
+        //            NumOfReports = 0,
+        //            DateCreated = DateTime.UtcNow,
+        //            DateUpdated = DateTime.UtcNow,
 
-                    UserId = newUser.Id
-                };
+        //            UserId = newUser.Id
+        //        };
 
-                var newPostWithImage = new Post()
-                {
-                    Content = "First post by PanmaTG lets go. 676767676. Image post baby",
+        //        var newPostWithImage = new Post()
+        //        {
+        //            Content = "First post by PanmaTG lets go. 676767676. Image post baby",
 
-                    ImageUrl = "https://static.toiimg.com/thumb/msid-120058075,width-1280,height-720,imgsize-65786,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
-                    NumOfReports = 0,
-                    DateCreated = DateTime.UtcNow,
-                    DateUpdated = DateTime.UtcNow,
+        //            ImageUrl = "https://static.toiimg.com/thumb/msid-120058075,width-1280,height-720,imgsize-65786,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+        //            NumOfReports = 0,
+        //            DateCreated = DateTime.UtcNow,
+        //            DateUpdated = DateTime.UtcNow,
 
-                    UserId = newUser.Id
-                };
+        //            UserId = newUser.Id
+        //        };
 
-                //To make this in async add "await keyword at the beginning here
-                //change AddRange() → AddRangeAsync();
-                //& change SaveChanges () → SaveChangesAsync();
-                await webAppDbContext.Posts.AddRangeAsync(newPostWithoutImage, newPostWithImage);
-                await webAppDbContext.SaveChangesAsync();
-            }
-        }
+        //        //To make this in async add "await keyword at the beginning here
+        //        //change AddRange() → AddRangeAsync();
+        //        //& change SaveChanges () → SaveChangesAsync();
+        //        await webAppDbContext.Posts.AddRangeAsync(newPostWithoutImage, newPostWithImage);
+        //        await webAppDbContext.SaveChangesAsync();
+        //    }
+        //}
     }
 }
--- PingedGu/Program.cs
+++ PingedGu/Program.cs
@@ -77,11 +77,12 @@ using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<WebAppDbContext>();
     await dbContext.Database.MigrateAsync();
-    await DbInitializer.SeedAsync(dbContext);
 
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<int>>>();
-     await DbInitializer.SeedUsersAndRolesAsync(userManager, roleManager);
+    await DbInitializer.SeedUsersAndRolesAsync(userManager, roleManager);
+
+    //await DbInitializer.SeedAsync(dbContext);
 }
     
 //---------------
